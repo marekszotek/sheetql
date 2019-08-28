3f49b54bde4ca7ba7d530636447743db3ca7c2af
mszotek <mszotek@gmail.com>
2019-08-28 11:05:45 +0200

    Setup project

---
+++ src/main/java/com/codecool/solosheetql/SoloSheetqlApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.solosheetql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloSheetqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoloSheetqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/solosheetql/SoloSheetqlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codecool.solosheetql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoloSheetqlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
