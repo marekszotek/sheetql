827ad0592f27d5abceb053086d92ee5b4d7390d6
mszotek <mszotek@gmail.com>
2019-08-28 14:42:59 +0200

    Implement dummy methods for table repository, controller and parser

---
+++ src/main/java/com/codecool/solosheetql/tables/TableParser.java
@@ -0,0 +1,12 @@
+package com.codecool.solosheetql.tables;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TableParser {
+
+
+    public String parseTableFromContent(String content) {
+        return content;
+    }
+}
