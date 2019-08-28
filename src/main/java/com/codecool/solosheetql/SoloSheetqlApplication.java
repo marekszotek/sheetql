f833f69d970127590ecb7e8e94b740cd9b2eb92c
mszotek <mszotek@gmail.com>
2019-08-28 21:10:25 +0200

    Remove command line runner from main, include tokens in gitignore

--- src/main/java/com/codecool/solosheetql/SoloSheetqlApplication.java
+++ src/main/java/com/codecool/solosheetql/SoloSheetqlApplication.java
@@ -1,12 +1,7 @@
 package com.codecool.solosheetql;
 
-import com.codecool.solosheetql.tables.TableLoader;
-import com.codecool.solosheetql.tables.TablesController;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SoloSheetqlApplication {
@@ -15,13 +10,13 @@ public class SoloSheetqlApplication {
         SpringApplication.run(SoloSheetqlApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner clr(ApplicationContext ctx) {
-        return args -> {
-            TablesController tablesController = ctx.getBean(TablesController.class);
-            System.out.println(tablesController.getTable("table"));
-
-        };
-    }
+//    @Bean
+//    public CommandLineRunner clr(ApplicationContext ctx) {
+//        return args -> {
+//            TablesController tablesController = ctx.getBean(TablesController.class);
+//            System.out.println(tablesController.getTable("table").display());
+//
+//        };
+//    }
 
 }
