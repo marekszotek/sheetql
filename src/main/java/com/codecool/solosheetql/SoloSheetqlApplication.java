ac61abe49d4f2902070a7b9b3db2f7a36acf9d89
mszotek <mszotek@gmail.com>
2019-08-28 14:42:18 +0200

    Implement Google spreadsheets connection methods in table loader

--- src/main/java/com/codecool/solosheetql/SoloSheetqlApplication.java
+++ src/main/java/com/codecool/solosheetql/SoloSheetqlApplication.java
@@ -1,13 +1,27 @@
 package com.codecool.solosheetql;
 
+import com.codecool.solosheetql.tables.TableLoader;
+import com.codecool.solosheetql.tables.TablesController;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SoloSheetqlApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SoloSheetqlApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner clr(ApplicationContext ctx) {
+        return args -> {
+            TablesController tablesController = ctx.getBean(TablesController.class);
+            System.out.println(tablesController.getTable("table"));
+
+        };
     }
 
 }
