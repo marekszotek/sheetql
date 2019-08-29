63407ef72c221e46278a7fbb5a69f73c8d4867da
mszotek <mszotek@gmail.com>
2019-08-29 09:46:06 +0200

    Implement map for google sheets repository

--- src/main/java/com/codecool/solosheetql/tables/TablesRepository.java
+++ src/main/java/com/codecool/solosheetql/tables/TablesRepository.java
@@ -2,10 +2,26 @@ package com.codecool.solosheetql.tables;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Repository
 public class TablesRepository {
 
+    Map<String, String> spreadsheetsIds;
+
+    public TablesRepository() {
+        this.spreadsheetsIds = loadSpreadsheetIds();
+    }
+
+    private Map<String, String> loadSpreadsheetIds() {
+        Map<String, String> spreadsheetIds = new HashMap<>();
+        spreadsheetIds.put("users", "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms");
+        spreadsheetIds.put("albums", "19y6w-X3xT4undl8bea99rGiKnUXQrP8EkGWAEFrUOUI");
+        return spreadsheetIds;
+    }
+
     String getSpreadsheetId(String spreadsheetName) {
-        return "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms";
+        return spreadsheetsIds.get(spreadsheetName);
     }
 }
