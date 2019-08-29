aa35916dd48e0f6205ca0caaf73b3480683e236c
mszotek <mszotek@gmail.com>
2019-08-29 23:21:43 +0200

    Test DevOps pipeline

--- src/main/java/com/codecool/solosheetql/tables/Table.java
+++ src/main/java/com/codecool/solosheetql/tables/Table.java
@@ -5,9 +5,14 @@ import java.util.stream.Collectors;
 
 public class Table {
 
+    private String title;
     private String name;
     private List<String> headers;
     private List<List<String>> records;
+
+    public Table() {
+        title = "DEVOPS TEST";
+    }
 
     public String getName() {
         return name;
