2bc03a06aefab613421736eefaad49f21874d3c6
mszotek <mszotek@gmail.com>
2019-09-06 13:06:40 +0200

    Handle GET instead of POST for mapping

--- src/main/java/com/codecool/solosheetql/httpcontroller/QueryController.java
+++ src/main/java/com/codecool/solosheetql/httpcontroller/QueryController.java
@@ -5,7 +5,7 @@ import com.codecool.solosheetql.tables.Table;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +22,7 @@ public class QueryController {
         this.queryRunner = queryRunner;
     }
 
-    @PostMapping
+    @GetMapping
     public ResponseEntity<Table> runQuery(@RequestBody Query query){
         try{
             return new ResponseEntity<>(queryRunner.getResultTableFromQuery(query.getText()), HttpStatus.OK);
